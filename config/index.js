fa61976709ac4731159c5ad75e7515ff4c1c29cd
Yordan <yordan@spjt.co.id>
2022-01-07 20:00:42 +0700

    update all

--- config/index.js
+++ config/index.js
@@ -1,8 +1,10 @@
 const dotenv = require('dotenv')
-
+const path = require('path')
 dotenv.config()
 
 module.exports = {
-    serviceName : process.env.SERVICE_NAME,
-    urlDb : process.env.MONGO_URL
+  rootPath: path.resolve(__dirname, '..'),
+  serviceName: process.env.SERVICE_NAME,
+  jwtKey: process.env.SECRET,
+  urlDb: process.env.MONGO_URL
 }
--- db/index.js
+++ db/index.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose')
 const { urlDb } = require('../config')
 
 mongoose.connect(urlDb, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+  useCreateIndex: true,
+  useNewUrlParser: true 
 })
 
-const db = mongoose.connection
+const db =  mongoose.connection
 
 module.exports = db
